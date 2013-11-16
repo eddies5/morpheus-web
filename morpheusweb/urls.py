57160c22723e7f0916206288b67e28c92db17b90
Bulat Bazarbayev <bazarbayev.b.k@gmail.com>
2013-11-15 18:17:06 -0600

    added mapping for submision

--- morpheusweb/urls.py
+++ morpheusweb/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from morpheus.views import job_submit
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^submit$', job_submit)
     # Examples:
     # url(r'^$', 'morpheusweb.views.home', name='home'),
     # url(r'^morpheusweb/', include('morpheusweb.foo.urls')),
