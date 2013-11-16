7b5e7bd927436d882b7a3861ace0314f4ca5a629
Bulat Bazarbayev <bazarbayev.b.k@gmail.com>
2013-11-15 18:14:53 -0600

    added JobEntry class

--- master.py
+++ master.py
@@ -4,3 +4,9 @@ class Master(object):
 
 	def __init__(self):
 		print("do nothing")
+
+
+class JobEntry(object):
+    def __init__(self, func, data):
+        self._func = func
+        self._data = data
