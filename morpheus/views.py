d76a7f179f00ac2191838986ac0899063be38546
Bulat Bazarbayev <bazarbayev.b.k@gmail.com>
2013-11-15 18:16:18 -0600

    added function submit_job, submit_job gets data from POST request and saves it into pickled file

--- morpheus/views.py
+++ morpheus/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from master import JobEntry
+import pickle
 
 # Create your views here.
+def job_submit(request):
+    if request.method == 'POST':
+        job = JobEntry(request.POST.get('function', None),
+                        request.POST.get('data'))
+        with open('object_file.p', 'w') as fout:
+            pickle.dump(job, fout)
+        return HttpResponse('job saved successfully')
+
+    return HttpResponse("Not a POST request")
