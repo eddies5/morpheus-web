08951755325a59066d9b3bcb35cbf94e3beb75b0
Bulat Bazarbayev <bazarbayev.b.k@gmail.com>
2013-11-15 18:48:18 -0600

    job_submit saves JobRecord into file and db

--- morpheus/views.py
+++ morpheus/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from master import JobEntry
 import pickle
+from morpheus.models import JobRecord
 
 # Create your views here.
 def job_submit(request):
     if request.method == 'POST':
         job = JobEntry(request.POST.get('function', None),
                         request.POST.get('data'))
-        with open('object_file.p', 'w') as fout:
+        #save file into object file
+        number_of_records = JobRecord.objects.filter().count()
+        file_name = 'job' + str(number_of_records)
+        print file_name
+        with open(file_name, 'w') as fout:
             pickle.dump(job, fout)
-        return HttpResponse('job saved successfully')
+        record = JobRecord(obj_name=file_name)
+        record.save()
+
+        return HttpResponse('job saved successfully job_id--->>' + str(number_of_records))
 
     return HttpResponse("Not a POST request")
 
