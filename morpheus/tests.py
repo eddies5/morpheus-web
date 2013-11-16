887a0a14cd6a7218dd01b57b59b05d9554c63025
Bulat Bazarbayev <bazarbayev.b.k@gmail.com>
2013-11-15 18:19:08 -0600

    tests fot submit_job funct in view

--- morpheus/tests.py
+++ morpheus/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+class JobSubmitTestCase(TestCase):
+    def test_job_submit(self):
+        c = Client()
+        response = c.post('/submit', {'function' : 'def semaple() { sample function}',
+            'data' : 'sample data here you go'})
+        print response.content
+
